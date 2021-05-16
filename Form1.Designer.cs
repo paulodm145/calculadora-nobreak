663a823a3db627c7091bd9a2a2c342a10465fe37
Paulo Roberto Bolsanello <paulo.bolsanello@infoa2.com.br>
2021-05-16 10:01:25 -0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace Calculadora_Nobreak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.NumericUpDown();
+            this.txtTensao = new System.Windows.Forms.TextBox();
+            this.txtCorrente = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPotencia = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.txtQuantidade)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Corrente da Bateria:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tensão da Bateria:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Quantidade de Bateria:";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantidade.Location = new System.Drawing.Point(16, 136);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(297, 26);
+            this.txtQuantidade.TabIndex = 3;
+            this.txtQuantidade.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // txtTensao
+            // 
+            this.txtTensao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTensao.Location = new System.Drawing.Point(16, 84);
+            this.txtTensao.Name = "txtTensao";
+            this.txtTensao.Size = new System.Drawing.Size(297, 26);
+            this.txtTensao.TabIndex = 4;
+            this.txtTensao.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tensao_KeyPress);
+            // 
+            // txtCorrente
+            // 
+            this.txtCorrente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCorrente.Location = new System.Drawing.Point(16, 32);
+            this.txtCorrente.Name = "txtCorrente";
+            this.txtCorrente.Size = new System.Drawing.Size(297, 26);
+            this.txtCorrente.TabIndex = 5;
+            this.txtCorrente.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 339);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(329, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(167, 17);
+            this.toolStripStatusLabel1.Text = "Tópicos Especiais de Hardware";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(16, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(297, 30);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPotencia
+            // 
+            this.txtPotencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPotencia.Location = new System.Drawing.Point(16, 188);
+            this.txtPotencia.Name = "txtPotencia";
+            this.txtPotencia.Size = new System.Drawing.Size(297, 26);
+            this.txtPotencia.TabIndex = 9;
+            this.txtPotencia.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.txtPotencia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.potencia_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(251, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Potência Consumida pelo Sistema";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblResultado.Location = new System.Drawing.Point(47, 278);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(234, 26);
+            this.lblResultado.TabIndex = 10;
+            this.lblResultado.Text = "Aguardando Resultado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 361);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtPotencia);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtCorrente);
+            this.Controls.Add(this.txtTensao);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            ((System.ComponentModel.ISupportInitialize)(this.txtQuantidade)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown txtQuantidade;
+        private System.Windows.Forms.TextBox txtTensao;
+        private System.Windows.Forms.TextBox txtCorrente;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPotencia;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_Nobreak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+        }
+
+        private void tensao_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+
+        private void potencia_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float autonomia, corrente, tensao, quantidade, potencia;
+
+            corrente = float.Parse(txtCorrente.Text);
+            tensao = float.Parse(txtTensao.Text);
+            quantidade = float.Parse(txtQuantidade.Text);
+            potencia = float.Parse(txtPotencia.Text);
+
+            /* Cálculo de autonomia */
+            autonomia =  corrente * tensao * quantidade / potencia;
+
+            /**Horas**/
+            float horas = (float)Math.Truncate(autonomia);
+
+            /*obter minutos*/
+            float minutesInDecimal = (autonomia - horas) * 60;
+            float minutes = (float)Math.Truncate((autonomia - horas)*60);
+
+            /* obter segundos */
+            float secondsInDecimal = minutesInDecimal - minutes;
+            float seconds = (float)Math.Truncate(secondsInDecimal*60);
+
+            lblResultado.Text = horas.ToString()+"h : "+minutes.ToString()+"m : "+ seconds.ToString()+"s";
+
+        }
+    }
+}
